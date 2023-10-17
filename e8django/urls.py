1a79fd09981e188bb5f41b7a3d79e92f6e6db4b9
vijin selvaraj <vijin@element8.ae>
2023-10-17 15:16:01 +0400

    initial setup

--- e8django/urls.py
+++ e8django/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for e8dajngo project.
+URL configuration for e8django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
