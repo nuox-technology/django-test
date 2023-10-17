dca1b22116c6bbc2c09d90785e9f4cff372bc418
varunnuox <148042325+varunnuox@users.noreply.github.com>
2023-10-17 22:35:45 +0530

    Update settings.py

--- e8django/settings.py
+++ e8django/settings.py
@@ -73,12 +73,16 @@ WSGI_APPLICATION = 'e8django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'postgres',
+          'USER': 'postgres',
+          'PASSWORD': 'postgres',
+          'HOST': 'db',
+          'PORT': 5432,
+      }
+  }
 
 
 # Password validation
